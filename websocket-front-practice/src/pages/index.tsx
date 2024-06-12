a57a4bdc910384b901fc26dd6f099f2815ec0922
Octoping <myc1365@naver.com>
2024-06-12 23:45:43 +0900

    Add files via upload

--- websocket-front-practice/src/pages/index.tsx
+++ websocket-front-practice/src/pages/index.tsx
@@ -1,5 +1,16 @@
 import { ChatRoom } from "@/component/ChatRoom";
+import { useState } from "react";
 
 export default function Home() {
-  return <ChatRoom />;
+  const [roomId, setRoomId] = useState<number>(2);
+
+  return (
+    <div>
+      <input
+        defaultValue={2}
+        onChange={(e) => setRoomId(Number(e.target.value))}
+      />
+      <ChatRoom roomId={roomId} />
+    </div>
+  );
 }
